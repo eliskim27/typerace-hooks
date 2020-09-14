c37c5fae1e30ca8c77d4b7104d131315c48c8b0a
eliskim27 <eliskim27@gmail.com>
2020-09-14 15:41:19 -0400

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [userText, setUserText] = useState('');
   return (
     <div>
       <h2>Type Race</h2>
