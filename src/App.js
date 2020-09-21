06a859ba87c03a75d560ac0e91aa6dc816af7fec
eliskim27 <eliskim27@gmail.com>
2020-09-21 10:51:44 -0400

    userText input field userState hook

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [userText, setUserText] = useState('');
+
+  const updateUserText = e => {
+    setUserText(e.target.value)
+    console.log('current userText', userText)
+  }
+
   return (
     <div>
       <h2>Type Race</h2>
-      <input/>
+      <input value={userText} onChange={updateUserText}/>
     </div>
   )
 }
+
 
 export default App;
