37f5416e9077b2cfdb5793857b14e5b1a0df9b3a
eliskim27 <eliskim27@gmail.com>
2020-09-21 11:55:29 -0400

    add text snippet strings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const [snippet, setSnippet] = useState('');
+  const [userText, setUserText] = useState('');
   const [userText, setUserText] = useState('');
 
   const updateUserText = e => {
     setUserText(e.target.value)
     console.log('current userText', userText)
   }
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+  };
 
   return (
     <div>
